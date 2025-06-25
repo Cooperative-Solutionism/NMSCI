3ccae15470282bd99d4d64bbc69add3732abd59a
KnowSkyAzure <96624375+AzureSkyProject@users.noreply.github.com>
2025-06-25 11:58:19 +0800

    reinit with springboot

---
+++ src/main/java/com/cooperativesolutionism/nmsci/NmsciApplication.java
@@ -0,0 +1,13 @@
+package com.cooperativesolutionism.nmsci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmsciApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NmsciApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cooperativesolutionism/nmsci/NmsciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooperativesolutionism.nmsci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmsciApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
